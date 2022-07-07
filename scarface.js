eed750d490c2b72735ab9a5df430353ed8f5af4e
scarfacedotcom <peteradaaku@gmail.com>
2022-07-07 12:33:31 +0100

    first commit

---
+++ scarface.js
@@ -0,0 +1,12 @@
+//FOR LOOP
+
+for(let i = 0; i < 5; i++){
+  console.log(i)
+}
+
+let names = ['joyce', 'peter', 'mabel']
+
+for (let i = 0; i < names.length; i++){
+  let html = `<div>${names[i]}</div>`;
+  console.log(html)
+}
