cf7f304614e4d40c1253cf472e3dacfae7369cda
scarfacedotcom <peteradaaku@gmail.com>
2022-07-07 13:04:59 +0100

    do while loops

--- scarface.js
+++ scarface.js
@@ -13,9 +13,18 @@
 
 //while loops
 
+// let i = 0;
+
+// while(i < 5) {
+//   console.log(i);
+//   i++
+// }
+
+//do while loop
+
 let i = 0;
 
-while(i < 5) {
+do {
   console.log(i);
   i++
-}
+} while(i < 5) 
