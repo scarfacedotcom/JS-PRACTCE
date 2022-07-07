3129c95c660756e4bd12f1bb33b6ad5fdf73e49a
scarfacedotcom <peteradaaku@gmail.com>
2022-07-07 12:42:46 +0100

    while loops

--- scarface.js
+++ scarface.js
@@ -1,12 +1,21 @@
 //FOR LOOP
 
-for(let i = 0; i < 5; i++){
-  console.log(i)
-}
+// for(let i = 0; i < 5; i++){
+//   console.log(i)
+// }
 
-let names = ['joyce', 'peter', 'mabel']
+// let names = ['joyce', 'peter', 'mabel']
 
-for (let i = 0; i < names.length; i++){
-  let html = `<div>${names[i]}</div>`;
-  console.log(html)
+// for (let i = 0; i < names.length; i++){
+//   let html = `<div>${names[i]}</div>`;
+//   console.log(html)
+// }
+
+//while loops
+
+let i = 0;
+
+while(i < 5) {
+  console.log(i);
+  i++
 }
